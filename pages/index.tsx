50a028794286281922e66d9cd3245dfebd70a969
Anas <amanabbasi3660@gmail.com>
2022-01-27 19:04:34 +0530

    implement the post section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import {sanityClient, urlFor} from "../sanity"
+import { Post } from '../typing'
 
-export default function Home() {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({posts}: Props) {
+
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -12,9 +20,48 @@ export default function Home() {
 
     <Header />
 
-    <div>
       <Banner />
-    </div>
+
+      {/* Posts */}
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 lg:p-6'>
+        {posts.map(post => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden ">
+              <img className='' src={urlFor(post.mainImage).url()!} alt="" />
+              <div className='flex justify-between bg-white p-5  items-center'>
+                <div>
+                  <p className='text-lg font-bold '>{post.title}</p>
+                  <p className="text-xs ">{post.description} by {post.author.name}</p>
+                </div>
+
+                <img src={urlFor(post.author.image).url()!} alt="" className='h-12 w-12 rounded-full border border-red-500 ' />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author-> {
+      name, 
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
